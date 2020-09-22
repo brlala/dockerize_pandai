7749c343c9dd782218086913710b02a6849e6c64
liheng <liheng@pand.ai>
2020-09-22 17:24:24 +0800

    completed mongo, redis, rabbtitmq, gateway

---
+++ pandai-gateway/entrypoint.sh
@@ -0,0 +1,18 @@
+FILE="/app/app.py"
+USERNAME=$1
+PASSWORD=$2
+if [ -f "$FILE" ]; then
+  # Take action if $FILE exists. #
+  echo "Repo exist, skipping steps"
+else
+  ### Control will jump here if doesn't exist ###
+  echo "Repo does not exist, skipping steps"
+  apt-get update
+  apt-get install -y git python-virtualenv
+  git init
+  git remote add origin https://"$USERNAME":"$PASSWORD"@bitbucket.org/pandaipteltd/pandai-gateway.git
+  git pull origin master
+  git reset --hard HEAD  # Required when the versioned files existed in path before "git init" of this repo.
+  git checkout master
+  pip install -r requirements.txt
+fi
